07ee7de5ecf5118ebef23b5952e68f9e9b90bc0e
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-08-01 20:23:31 -0400

    Add and remove item functions, modal functions, state variables

--- src/app/page.js
+++ src/app/page.js
@@ -28,15 +28,47 @@ const style = {
   gap: 3,
 }
 
+// Modal functions
+const handleOpen = () => setOpen(true)
+const handleClose = () => setOpen(false)
+
 export default function Home() {
-  // We'll add our component logic here
+  // State management 
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
 
+  // Component logic
   return (
     <Box>
       <Typography variant="h1">Inventory Management</Typography>
     </Box>
   )
+}
+
+// Add and remove item functions
+const addItem = async (item) => {
+  const docRef = doc(collection(firestore, 'inventory'), item)
+  const docSnap = await getDoc(docRef)
+  if (docSnap.exists()) {
+    const { quantity } = docSnap.data()
+    await setDoc(docRef, { quantity: quantity + 1})
+  } else {
+    await setDoc(docRef, { quantity: 1})
+  }
+  await updateInventory()
+}
+
+const removeItem = async (item) => {
+  const docRef = doc(collection(firestore, 'inventory'), item)
+  const docSnap = await getDoc(docRef)
+  if (docSnap.exists()) {
+    const { quantity } = docSnap.data()
+    if (quantity === 1) {
+      await deleteDoc(docRef)
+    } else {
+      await setDoc(docRef, { quantity: quantity - 1 })
+    }
+  }
+  await updateInventory()
 }
